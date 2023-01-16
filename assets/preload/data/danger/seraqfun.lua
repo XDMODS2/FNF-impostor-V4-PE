bc294fe1aa232776079eaa5039cc168a2b604cf5
XDMODS2 <121914323+XDMODS2@users.noreply.github.com>
2023-01-15 23:26:38 -0300

    Add files via upload

---
+++ assets/preload/data/danger/seraqfun.lua
@@ -0,0 +1,5 @@
+function onCreate()
+addCharacterToList('blackalt', 'dad') 
+addCharacterToList('black-run', 'dad') 
+addCharacterToList('bf-running', 'boyfriend') 
+end
---
+++ assets/preload/data/danger/texto y remoto.lua
@@ -0,0 +1,3 @@
+function onCreatePost()
+    doTweenColor('scoreTxt', 'scoreTxt', '000000', 0.01, 'linear');
+end
